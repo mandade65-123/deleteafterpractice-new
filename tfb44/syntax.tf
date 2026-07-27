172150a56371f8526d409d6174658aa0f5af744a
mandade65-123 <mandade65@gmail.com>
2026-07-27 14:21:49 +0530

    s

--- tfb44/syntax.tf
+++ tfb44/syntax.tf
@@ -16,7 +16,7 @@ resource "cloudprovider_resource" "unique_blockname" {
      arguements 
 }
 
-resource "aws_instance"  "webserver1" {
+resource "aws_instance"  "webserver2" {
      ami = 
      instance_type =
      vpc_security_group_ids = 
