6044d3c82312f0e526c7e71e4b87977abf4c3aee
mandade65-123 <mandade65@gmail.com>
2026-07-29 11:50:27 +0530

    d

--- tfb44/ec2.tf
+++ tfb44/ec2.tf
@@ -7,36 +7,36 @@ provider "aws" {
 
 
 resource "aws_instance"  "webserver" {
-    ami = "ami-004f790b835b26145" 
+    ami = "ami-004f790b835b26145"
     instance_type = "t3.micro"
-    vpc_security_group_ids =  ["sg-0eae0d103615165a6" , aws_security_group. webserver_sg.id]
+    vpc_security_group_ids =  ["sg-0eae0d103615165a6" , aws_security_group.webserver_sg.id ]
     key_name = "key"
     tags = {
         purpose = "webserver"
     }
-    count = 2 
-    user_data = <<- EOF 
-                 #!bin/bash
+    count = 2
+    user_data = <<-EOF
+                 #!/bin/bash
                  sudo yum install nginx -y
                  sudo systemctl start nginx
 
 
-                 EOF
 
-
+                EOF
 
 }
 
-resource "aws_security_group"  "webserver_sg" {
+
+resource "aws_security_group" "webserver_sg" {
     name = "tf-sg"
-     ingress {
-       from_port = 80
-       to_port = 80
-       protocol  = "TCP"
-       cidr_blocks = ["0.0.0.0/0"]
-}
-}
-     ingress {
+    ingress {
+      from_port = 80
+      to_port = 80
+      protocol = "TCP"
+      cidr_blocks  = ["0.0.0.0/0"]
+    }
+
+    ingress {
       from_port = 0
       to_port = 0
       protocol = "-1"
@@ -49,7 +49,7 @@ resource "aws_security_group"  "webserver_sg" {
       to_port = 0
       protocol = "-1"
       cidr_blocks  = ["0.0.0.0/0"]
+
     }
 
-
-
+}
