e844e992f6be06a566ead27974cfd625f664c977
mandade65-123 <mandade65@gmail.com>
2026-07-29 11:46:16 +0530

    a

--- tfb44/ec2.tf
+++ tfb44/ec2.tf
@@ -37,17 +37,19 @@ resource "aws_security_group"  "webserver_sg" {
 }
 }
      ingress {
-       from_port = 0
-       to_port   = 65535
-       protocol  = "-1"
-       cidr_blocks = ["0.0.0.0/0"]
-}
-     egress {
       from_port = 0
-      to_port   = 65535
-      protocol  = "-1"
-      cidr_blocks = ["0.0.0.0/0"]      
-}
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks  = ["0.0.0.0/0"]
+    }
+
+
+    egress {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks  = ["0.0.0.0/0"]
+    }
 
 
 
