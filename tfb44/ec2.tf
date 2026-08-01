1b2769a8eca72e9f9e0f58a88625ff05b005feec
mandade65-123 <mandade65@gmail.com>
2026-08-01 19:33:53 +0530

    s

--- tfb44/ec2.tf
+++ tfb44/ec2.tf
@@ -55,10 +55,11 @@ data "aws_security_group" "sgdata" {
 
 
 output "instaneip" {
-   value = aws_instance.webserver.public_ip
+  value = aws_instance.webserver[0].public_ip
 }
 
 output "pubdns" {
-  value = aws_instance.webserver.public_dns
+  value = aws_instance.webserver[0].public_dns
 }
+
 
