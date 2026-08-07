4b5db540d21d110f34d4b77dae02a70283e0ff56
mandade65-123 <mandade65@gmail.com>
2026-08-07 13:12:25 +0530

    s

---
+++ tfb44/remote/remote.tf
@@ -0,0 +1,30 @@
+provider "aws" {
+  region = "ap-south-1"
+  profile = "configs"
+}
+
+#Partitionkey = LockID
+terraform {
+    backend "s3" {
+        bucket = "cbb44"
+        key = "terraform.tfstate"
+        dynamodb_table = "cbb44"
+        region = "ap-south-1"
+        profile = "configs"
+        shared_credentials_files = ["/home/nayan123/.aws/credentials"]
+    }
+}
+
+
+
+
+
+
+
+
+resource "aws_instance"  "webserver" {
+     ami = var.amiid
+     instance_type =  var.insttype
+     vpc_security_group_ids = [ var.sg ]
+     key_name = var.kp
+}
