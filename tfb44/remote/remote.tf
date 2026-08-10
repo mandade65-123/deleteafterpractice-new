f6dbad3fb4868080ef86b38c6f956a76e384dbb5
mandade65-123 <mandade65@gmail.com>
2026-08-10 11:59:35 +0530

    a

--- tfb44/remote/remote.tf
+++ tfb44/remote/remote.tf
@@ -6,9 +6,9 @@ provider "aws" {
 #Partitionkey = LockID
 terraform {
     backend "s3" {
-        bucket = "cbb44"
+        bucket = "cb-b44"
         key = "terraform.tfstate"
-        dynamodb_table = "b44"
+        dynamodb_table = "cb-b44"
         region = "ap-south-1"
         profile = "configs"
         shared_credentials_files = ["/home/nayan123/.aws/credentials"]
