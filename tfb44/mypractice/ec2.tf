6ceaf4cf341236e661a3ed98813d07ee8ceb2c83
mandade65-123 <mandade65@gmail.com>
2026-08-11 13:38:17 +0530

    s

--- tfb44/mypractice/ec2.tf
+++ tfb44/mypractice/ec2.tf
@@ -14,6 +14,12 @@ resource "aws_instance"  "webserver" {
          purpose = "webserver"   
      }
      count = 2
+     disable_api_termination = var.apiterm
+     user_data = <<-EOF
+                 #!/bin/bash
+                 sudo yum install nginx -y
+                 sudo systemctl start nginx
+                EOF
 }
 
 resource "aws_security_group" "webserver_sg" {
