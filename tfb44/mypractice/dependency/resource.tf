312aa082042f771f5ddc536734889a41593f8763
mandade65-123 <mandade65@gmail.com>
2026-08-13 14:22:38 +0530

    s

--- tfb44/mypractice/dependency/resource.tf
+++ tfb44/mypractice/dependency/resource.tf
@@ -14,7 +14,7 @@ resource "aws_instance"  "webserver" {
 
 
 resource "aws_security_group" "webserver_sg" {
-    depends_on = [aws_instance.webserver]    
+  depends_on = aws_instance.webserver
     name = "tf-sg2"
     ingress {
       from_port = 80
@@ -37,9 +37,9 @@ resource "aws_security_group" "webserver_sg" {
       protocol = "-1"
       cidr_blocks  = ["0.0.0.0/0"]
 
-    }
-
+     }
 }
+
 
 
 
