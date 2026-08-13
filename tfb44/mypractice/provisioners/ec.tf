f56263b50a2ba383314b6170bc9f913ffbdabb75
mandade65-123 <mandade65@gmail.com>
2026-08-13 10:36:52 +0530

    a

--- tfb44/mypractice/provisioners/ec.tf
+++ tfb44/mypractice/provisioners/ec.tf
@@ -1,28 +1,32 @@
 provider "aws" {
-    region = "us-east-1"
-   profile = "configs"
-}
+  region = "us-east-1"
+  profile = "configs"
+} 
 
 resource "aws_instance"  "webserver" {
     ami = var.amiid
     instance_type = var.insttype
     vpc_security_group_ids =  [ var.sg]
     key_name = var.kp
-}
-connection {
-    type        = "ssh"
-    user        = "ec2-user"
-    private_key = file("${path.module}/tf.pem")
-    host        = self.public_ip
-  }
+
+
 provisioner "file" {
     source      = "sample.txt"
     destination = "/home/ec2-user/aws/"
   }
 
+provisioner "local-exec" {
+    command = "echo ${self.private_ip} >> private_ips.txt" 
+}
 
+ connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file("${path.module}/tf.pem")
+    host        = self.public_ip
+  }
 
-   provisioner "remote-exec" {
+ provisioner "remote-exec" {
     inline = [
 
       "sudo yum install httpd -y",
@@ -33,6 +37,8 @@ provisioner "file" {
     ]
   }
 
-provisioner "local-exec" {
-    command = "echo ${self.private_ip} >> private_ips.txt" 
+
+
+
+
 }
