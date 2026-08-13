2f05bf035a099e6921d6e612159569639714c0a4
mandade65-123 <mandade65@gmail.com>
2026-08-13 11:41:02 +0530

    s

--- tfb44/mypractice/provisioners/ec.tf
+++ tfb44/mypractice/provisioners/ec.tf
@@ -1,47 +1,36 @@
 provider "aws" {
-  region  = "us-east-1"
+  region = "ap-south-1"
   profile = "configs"
 }
+resource "aws_instance"  "webserver" {
+     ami = var.amiid
+     instance_type =  var.insttype
+     vpc_security_group_ids = [ var.sg ]
+     key_name = var.kp
+     
+provisioner "file" {
+    source      = "sample.txt"
+    destination = "/home/ec2-user/aws/"
+  }
 
-resource "aws_instance" "webserver" {
-  ami                    = var.amiid
-  instance_type          = var.insttype
-  vpc_security_group_ids = [var.sg]
-  key_name               = var.key # Ensure this variable matches your .tfvars
+provisioner "local-exec" {
+    command = "echo ${self.private_ip} >> private_ips.txt" 
+}
 
-  # STEP 1: Define connection first. Terraform loads tf.pem into memory here.
-  connection {
+ connection {
     type        = "ssh"
     user        = "ec2-user"
     private_key = file("${path.module}/tf.pem")
     host        = self.public_ip
-    timeout     = "5m" # Gives AWS enough time to boot up the SSH service
   }
 
-  # STEP 2: Create the target directory so the file transfer doesn't crash
-  provisioner "remote-exec" {
+ provisioner "remote-exec" {
     inline = [
-      "mkdir -p /home/ec2-user/aws/"
-    ]
-  }
 
-  # STEP 3: Transfer the file safely using the connection loaded in Step 1
-  provisioner "file" {
-    source      = "sample.txt"
-    destination = "/home/ec2-user/aws/sample.txt"
-  }
-
-  # STEP 4: Run local script logging
-  provisioner "local-exec" {
-    command = "echo ${self.private_ip} >> private_ips.txt"
-  }
-
-  # STEP 5: Install and configure Apache
-  provisioner "remote-exec" {
-    inline = [
-      "sudo yum update -y",
       "sudo yum install httpd -y",
       "sudo systemctl start httpd",
+      "sudo yum update all",
+      "sudo yum upgrade",
       "sudo systemctl enable httpd"
     ]
   }
--- tfb44/mypractice/provisioners/var.tf
+++ tfb44/mypractice/provisioners/var.tf
@@ -1,15 +1,23 @@
 variable "amiid" {
-    default = "ami-004f790b835b26145"
+    default =  "ami-035827357e3c7e810"
 }
-
 variable "insttype" {
     default = "t3.micro"
 }
 
 variable "sg" {
-   default = "sg-0eae0d103615165a6"
+    default = "sg-05e7e2c67090bfab2"
 }
 
-variable "key" {
+
+variable "kp" {
     default = "key"
+}
+
+variable "instno" {
+    default = 2
+}
+
+variable "apiterm" {
+    default = false
 }
