73b84f172066812379de4580dac570641bc877c7
mandade65-123 <mandade65@gmail.com>
2026-08-13 15:45:48 +0530

    s

--- tfb44/dependency/resource.tf
+++ tfb44/dependency/resource.tf
@@ -11,6 +11,33 @@ resource "aws_instance"  "webserver" {
     key_name = var.kp
     count = 2  #identicalloops
 }
+resource "aws_security_group" "webserver_sg" {
+    depends_on = [aws_instance.webserver]    
+    name = "tf-sg2"
+    ingress {
+      from_port = 80
+      to_port = 80
+      protocol = "TCP"
+      cidr_blocks  = ["0.0.0.0/0"]
+    }
+
+    ingress {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks  = ["0.0.0.0/0"]
+    }
+
+
+    egress {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks  = ["0.0.0.0/0"]
+
+    }
+
+}
 
 
 
