45a33f42ea35519cca28957f0a73de012ee9904e
mandade65-123 <mandade65@gmail.com>
2026-08-13 10:49:06 +0530

    s

--- tfb44/mypractice/provisioners/ec.tf
+++ tfb44/mypractice/provisioners/ec.tf
@@ -1,44 +1,37 @@
 provider "aws" {
-  region = "us-east-1"
+  region  = "us-east-1"
   profile = "configs"
-} 
-
-resource "aws_instance"  "webserver" {
-    ami = var.amiid
-    instance_type = var.insttype
-    vpc_security_group_ids =  [ var.sg]
-    key_name = var.kp
-
-
-provisioner "file" {
-    source      = "sample.txt"
-    destination = "/home/ec2-user/aws/"
-  }
-
-provisioner "local-exec" {
-    command = "echo ${self.private_ip} >> private_ips.txt" 
 }
 
- connection {
+resource "aws_instance" "webserver" {
+  ami                    = var.amiid
+  instance_type          = var.insttype
+  vpc_security_group_ids = [var.sg]
+  key_name               = var.kp
+
+  # Connection block placed here applies to all remote provisioners
+  connection {
     type        = "ssh"
     user        = "ec2-user"
     private_key = file("${path.module}/tf.pem")
     host        = self.public_ip
   }
 
- provisioner "remote-exec" {
-    inline = [
+  provisioner "file" {
+    source      = "sample.txt"
+    destination = "/home/ec2-user/aws/sample.txt" 
+  }
 
+  provisioner "local-exec" {
+    command = "echo ${self.private_ip} >> private_ips.txt"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum update -y",
       "sudo yum install httpd -y",
       "sudo systemctl start httpd",
-      "sudo yum update all",
-      "sudo yum upgrade",
       "sudo systemctl enable httpd"
     ]
   }
-
-
-
-
-
 }
