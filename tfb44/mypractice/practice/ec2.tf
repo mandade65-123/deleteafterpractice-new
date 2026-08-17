695976c1d8d3e7540b5ece4e8259195ac950f5a0
mandade65-123 <mandade65@gmail.com>
2026-08-17 10:40:34 +0530

    s

--- tfb44/mypractice/practice/ec2.tf
+++ tfb44/mypractice/practice/ec2.tf
@@ -15,3 +15,28 @@ resource "aws_instance" "webserver" {
     }
     count =2
 }
+resource "aws_security_group" "webserver_sg" {
+name = "tf-sg"
+    ingress {
+      from_port = 80
+      to_port = 80
+      protocol = "TCP"
+      cidr_blocks  = ["0.0.0.0/0"]
+    }
+
+    ingress {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks  = ["0.0.0.0/0"]
+    }
+
+
+    egress {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks  = ["0.0.0.0/0"]
+
+     }
+}
