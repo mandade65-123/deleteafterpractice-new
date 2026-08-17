3d72ae31937d849b2d992d4346e2a0d20cc30287
mandade65-123 <mandade65@gmail.com>
2026-08-17 10:22:50 +0530

    a

--- tfb44/mypractice/practice/ec2.tf
+++ tfb44/mypractice/practice/ec2.tf
@@ -8,7 +8,7 @@ provider "aws" {
 resource "aws_instance" "webserver" {
     ami = "ami-0bdc7d025135d7b49"
     instance_type = "t3.micro"
-    vpc_security_group_ids = ["sg-0dce926e31d5c67c1"]
+    vpc_security_group_ids = ["sg-0eae0d103615165a6"]
     key_name = "key"
     tags = {
         purpose = "webserver"
