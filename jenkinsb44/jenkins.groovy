ad9b586eb14c2c6bb819b7b6796e716ae397ac57
mandade65-123 <mandade65@gmail.com>
2026-08-20 13:31:45 +0530

    s

---
+++ jenkinsb44/jenkins.groovy
@@ -0,0 +1,26 @@
+pipeline {
+    agent any 
+    stages {
+        stage('Pull') {
+            steps {
+                sh 'echo "this is pull stage"'
+                git 'https://github.com/mandade65-123/studentapp-ui.git'
+            }
+        }
+        stage('Build') {
+            steps {
+                sh 'echo "this is Build stage"'
+            }
+        }
+        stage('Test') {
+            steps {
+                sh 'echo "this is Test stage"'
+            }
+        }
+        stage('Deploy') {
+            steps {
+                sh 'echo "this is Deploy stage"'
+            }
+        }
+    }
+}
