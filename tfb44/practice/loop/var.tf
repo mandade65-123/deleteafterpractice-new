d6bbed359ba5498072fdde25b85b43f652353980
mandade65-123 <mandade65@gmail.com>
2026-08-21 16:21:16 +0530

    a

---
+++ tfb44/practice/loop/var.tf
@@ -0,0 +1,16 @@
+variable "amiid" {
+    default = "ami-035827357e3c7e810"
+}
+
+variable "insttype" {
+    default = "t3.micro"
+}
+
+variable "sg" {
+   default = "sg-05e7e2c67090bfab2"
+}
+
+variable "kp" {
+    default = "key"
+
+}
