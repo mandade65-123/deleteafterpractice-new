fe2e31832a2fcad3e8ffd3039dc929171e6ec023
mandade65-123 <mandade65@gmail.com>
2026-08-21 09:40:17 +0530

    s

--- tfb44/practice/ec2.tf
+++ tfb44/practice/ec2.tf
@@ -8,10 +8,36 @@ provider "aws" {
 resource "aws_instance" "webserver" {
     ami = "ami-0332d564d76dbd8d6"
     instance_type = "t3.micro"
-    vpc_security_group_ids = ["sg-0eae0d103615165a6"]
+    vpc_security_group_ids = ["sg-0eae0d103615165a6",aws_security_group.webserver_sg.id]
     key_name = "key"
     tags ={
         purpose = "webserver"
     }
     count = 1
     }
+
+   resource "aws_security_group" "webserver_sg" {
+    name = "tf-sg-group"
+    ingress {
+      from_port = 80
+      to_port = 80
+      protocol = "TCP"
+      cidr_blocks  = ["0.0.0.0/0"]
+    }
+
+    ingress {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks  = ["0.0.0.0/0"]
+    }
+
+
+    egress {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks  = ["0.0.0.0/0"]
+
+     }
+}
