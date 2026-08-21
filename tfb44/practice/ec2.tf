4590ef37beab0c67d6bb8dd54030beceec38ebb4
mandade65-123 <mandade65@gmail.com>
2026-08-21 09:18:50 +0530

    s

---
+++ tfb44/practice/ec2.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+   region = "us-east-1"
+   access_key = "my-access-key"
+   secret_key = "my-secret-key"   
+
+}
+
+resource "aws_instance" "webserver"
+    ami = "ami-0332d564d76dbd8d6"
+    instance_type = "t3.micro"
+    vpc_security_group_ids = ["sg-0eae0d103615165a6"]
+    key_name = "key"
