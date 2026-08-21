15f034c9e106436eb1b651880018be93dd97e71a
mandade65-123 <mandade65@gmail.com>
2026-08-21 10:03:27 +0530

    s

--- tfb44/practice/ec2.tf
+++ tfb44/practice/ec2.tf
@@ -6,14 +6,15 @@ provider "aws" {
 }
 
 resource "aws_instance" "webserver" {
-    ami = "ami-0332d564d76dbd8d6"
-    instance_type = "t3.micro"
-    vpc_security_group_ids = ["sg-0eae0d103615165a6",aws_security_group.webserver_sg.id]
-    key_name = "key"
+    ami = var.amiid
+    instance_type = var.insttype
+    vpc_security_group_ids = [var.sg ,aws_security_group.webserver_sg.id]
+    key_name = var.kp
     tags ={
         purpose = "webserver"
     }
-    count = 1
+    count = var.instno
+     disable_api_termination = var.apiterm
      user_data = <<-EOF
                  #!/bin/bash
                  sudo yum install nginx -y
