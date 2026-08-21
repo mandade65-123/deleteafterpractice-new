ccfad96ce8981facf76496f88a688c98a47a0c71
mandade65-123 <mandade65@gmail.com>
2026-08-21 15:44:22 +0530

    s

---
+++ tfb44/practice/provisioners/var.tf
@@ -0,0 +1,23 @@
+variable "amiid" {
+    default =  "ami-035827357e3c7e810"
+}
+variable "insttype" {
+    default = "t3.micro"
+}
+
+variable "sg" {
+    default = "sg-05e7e2c67090bfab2"
+}
+
+
+variable "kp" {
+    default = "key"
+}
+
+variable "instno" {
+    default = 2
+}
+
+variable "apiterm" {
+    default = false
+}
