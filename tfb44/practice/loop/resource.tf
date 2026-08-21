b5f9a25b78cf9e6a51c46ee50e4809cdac4829ea
mandade65-123 <mandade65@gmail.com>
2026-08-21 19:58:38 +0530

    s

--- tfb44/practice/loop/resource.tf
+++ tfb44/practice/loop/resource.tf
@@ -1,31 +1,37 @@
 provider "aws" {
-   region = "us-east-1"
-   profile = "configs"
-}
+  region = "us-east-1"
+  profile = "configs"
+} 
 
-resource "aws_instance" "webserver" {
+
+resource "aws_instance"  "webserver" {
     ami = var.amiid
     instance_type = var.insttype
-    vpc_security_group_ids = [var.sg  ]
+    vpc_security_group_ids =  [ var.sg ]
     key_name = var.kp
-    count =2
+    count = 2  #identicalloops
 }
-resource "aws_instance" "webservertwo" {
-    for_each = toset(var.imageid)
+
+
+
+resource "aws_instance"  "webservertwo" {
+    for_each = toset(var.imageid)    #unidenticalloops
     ami = each.value
     instance_type = var.insttype
-    vpc_security_group_ids = [var.sg  ]
+    vpc_security_group_ids =  [ var.sg]
     key_name = var.kp
-    count = 3
+
 }
 
 
-variable "imageid"  {
-   default = ["ami-0332d564d76dbd8d6" , "ami-0b6d9d3d33ba97d99" , "ami-05b8af58f7410b671"]
+
+
+variable "imageid" {
+   default = ["ami-0b6d9d3d33ba97d99" , "ami-02b3d83d84b07786d " , "ami-05b8af58f7410b671 "]
 }
 
 output "ip" {
-    value = [ for amiid in var.imageid
-             aws_instance.webservertwo[amiid].public_ip]
-      
+    value = [
+      for amiid in var.imageid:  
+        aws_instance.webservertwo[amiid].public_ip]
 }
