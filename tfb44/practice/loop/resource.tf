67649ba12a5a63f8464c4f9bc03d70a1d0d82967
mandade65-123 <mandade65@gmail.com>
2026-08-21 19:44:24 +0530

    a

---
+++ tfb44/practice/loop/resource.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+   region = "us-east-1"
+   profile = "configs"
+}
+
+resource "aws_instance" "webserver" {
+    ami = var.amiid
+    instance_type = var.insttype
+    vpc_security_group_ids = [var.sg  ]
+    key_name = var.kp
+    count =2
+}
+resource "aws_instance" "webserver" {
+    for_each = toset(var.imageid)
+    ami = each.value
+    instance_type = var.insttype
+    vpc_security_group_ids = [var.sg  ]
+    key_name = var.kp
+    count = 3
+}
+
+
+variable "imageid"  {
+   default = ["ami-0332d564d76dbd8d6" , "ami-0b6d9d3d33ba97d99" , "ami-05b8af58f7410b671"]
+}
