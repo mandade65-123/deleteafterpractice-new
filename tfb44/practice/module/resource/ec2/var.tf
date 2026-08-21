d35b303ac868ebe258d8c742a9edf76ee3e76d9c
mandade65-123 <mandade65@gmail.com>
2026-08-21 13:01:58 +0530

    s

--- tfb44/practice/module/resource/ec2/var.tf
+++ tfb44/practice/module/resource/ec2/var.tf
@@ -1,21 +1,21 @@
 variable "amiid" {
-    default = 
+   
 }
 
 variable "insttype" {
-    default = 
+  
 }
 
 variable "sg" {
-    default = 
+   
 }
 
 variable "kp" {
-    default = 
+   
 }
 
 variable "apiterm" {
-    default = 
+   
 }
 
 variable "ws_subnet" {
