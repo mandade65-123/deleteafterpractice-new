6ee636d82250a316d96bf2f3592b323ba5e0d7a3
mandade65-123 <mandade65@gmail.com>
2026-08-21 20:11:58 +0530

    a

---
+++ tfb44/practice/eks/eks.tf
@@ -0,0 +1,116 @@
+provider "aws" { 
+  region  = "us-east-1" 
+  profile = "configs" 
+}
+
+# 1. Fetch the default VPC ID
+data "aws_vpc" "default" { 
+  default = true 
+}
+
+# 2. Automatically discover all default subnets across multiple AZs
+data "aws_subnets" "default_subnets" { 
+  filter { 
+    name   = "vpc-id" 
+    values = [data.aws_vpc.default.id] 
+  } 
+  filter { 
+    name   = "default-for-az" 
+    values = ["true"] 
+  } 
+}
+
+# NEW: Generates a unique tracking suffix to avoid IAM 409 errors
+resource "random_string" "suffix" {
+  length  = 6
+  special = false
+  upper   = false
+}
+
+# 3. EKS Cluster IAM Role
+resource "aws_iam_role" "eks_role" { 
+  name = "eks-cluster-role-${random_string.suffix.result}" 
+  assume_role_policy = jsonencode({ 
+    Version = "2012-10-17" 
+    Statement = [{ 
+      Action    = "sts:AssumeRole" 
+      Effect    = "Allow" 
+      Principal = { Service = "eks.amazonaws.com" } 
+    }] 
+  }) 
+}
+
+resource "aws_iam_role_policy_attachment" "eks_policy_attachment" { 
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy" 
+  role       = aws_iam_role.eks_role.name 
+}
+
+# 4. EKS Cluster Configuration
+resource "aws_eks_cluster" "eks" { 
+  name     = "my-eks-cluster" 
+  role_arn = aws_iam_role.eks_role.arn 
+  
+  vpc_config { 
+    subnet_ids = data.aws_subnets.default_subnets.ids 
+  } 
+  
+  depends_on = [aws_iam_role_policy_attachment.eks_policy_attachment] 
+}
+
+# 5. EKS Worker Nodes IAM Role
+resource "aws_iam_role" "eks_node_role" { 
+  name = "eks-node-group-role-${random_string.suffix.result}" 
+  assume_role_policy = jsonencode({ 
+    Version = "2012-10-17" 
+    Statement = [{ 
+      Action    = "sts:AssumeRole" 
+      Effect    = "Allow" 
+      Principal = { Service = "ec2.amazonaws.com" } 
+    }] 
+  }) 
+}
+
+resource "aws_iam_role_policy_attachment" "eks_worker_node_policy" { 
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy" 
+  role       = aws_iam_role.eks_node_role.name 
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cni_policy" { 
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy" 
+  role       = aws_iam_role.eks_node_role.name 
+}
+
+resource "aws_iam_role_policy_attachment" "eks_ec2_container_registry_policy" { 
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly" 
+  role       = aws_iam_role.eks_node_role.name 
+}
+
+# 6. EKS Managed Node Group Configuration
+resource "aws_eks_node_group" "eks_nodes" { 
+  cluster_name    = aws_eks_cluster.eks.name 
+  node_group_name = "eks-node-group" 
+  node_role_arn   = aws_iam_role.eks_node_role.arn 
+  subnet_ids      = data.aws_subnets.default_subnets.ids 
+  instance_types  = ["c7i-flex.large"] 
+  
+  scaling_config { 
+    desired_size = 2 
+    max_size     = 3 
+    min_size     = 1 
+  } 
+  
+  depends_on = [ 
+    aws_iam_role_policy_attachment.eks_worker_node_policy, 
+    aws_iam_role_policy_attachment.eks_cni_policy, 
+    aws_iam_role_policy_attachment.eks_ec2_container_registry_policy 
+  ] 
+}
+
+# 7. Outputs
+output "cluster_endpoint" { 
+  value = aws_eks_cluster.eks.endpoint 
+}
+
+output "node_group_name" { 
+  value = aws_eks_node_group.eks_nodes.node_group_name 
+}
