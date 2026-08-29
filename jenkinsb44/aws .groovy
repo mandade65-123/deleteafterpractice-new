b8dcda7473ecae23b0ca99714d79206efa5d9598
mandade65-123 <mandade65@gmail.com>
2026-08-29 09:58:31 +0530

    s

--- jenkinsb44/aws .groovy
+++ jenkinsb44/aws .groovy
@@ -2,3 +2,33 @@
 #curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
+
+#Connect to Your Jenkins Server
+ssh user@your-jenkins-server-ip
+
+
+ #Install eksctl
+ # 1. Download and extract the latest Linux AMD64 binary into /tmp
+curl --silent --location "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+
+# 2. Move the binary into your global binary folder
+sudo mv /tmp/eksctl /usr/local/bin
+
+# 3. Verify the installation
+eksctl version
+
+
+#Install kubectl
+# 1. Download the latest stable native Linux binary
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+
+# 2. Install it with execution permissions to /usr/local/bin
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+# 3. Verify the installation
+kubectl version --client
+
+
+# Test permissions as the jenkins system user
+sudo -u jenkins eksctl version
+sudo -u jenkins kubectl version --client
