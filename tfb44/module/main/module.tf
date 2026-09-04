aeaecf59caa5d1fe8c58fe38d8369d55e22887b6
mandade65-123 <mandade65@gmail.com>
2026-09-04 16:27:07 +0530

    s

--- tfb44/module/main/module.tf
+++ tfb44/module/main/module.tf
@@ -4,7 +4,7 @@ module "ec2" {
     source = "/root/deleteafterpractice-new/tfb44/module/resource/ec2"
     amiid =  "ami-090d68841c2a28756"
     insttype = "t3.micro"
-    sg = "sg-05e7e2c67090bfab2"
+    sg = "sg-0b2d72b32c63f6b64"
     kp = "key"
     apiterm = false
    ws_subnet = module.vpc.subnet_id
