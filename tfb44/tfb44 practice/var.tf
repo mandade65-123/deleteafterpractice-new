8a89cddf089c7b1a5da71d03ead0d17416d4e845
mandade65-123 <mandade65@gmail.com>
2026-09-04 15:14:22 +0530

    s

---
+++ tfb44/tfb44 practice/var.tf
@@ -0,0 +1,23 @@
+variable "amiid" {
+    default =  "ami-090d68841c2a28756"
+}
+variable "insttype" {
+    default = "t3.micro"
+}
+
+variable "sg" {
+    default = "sg-0bfd84aa822c5da68"
+}
+
+
+variable "kp" {
+    default = "key"
+}
+
+variable "instno" {
+    default = 2
+}
+
+variable "apiterm" {
+    default = false
+}
