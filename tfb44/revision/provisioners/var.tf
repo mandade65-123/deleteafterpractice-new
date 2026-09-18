f23ca02657584f28fe831e26523f5ac7db59a11a
mandade65-123 <mandade65@gmail.com>
2026-09-18 16:40:59 +0530

    s

---
+++ tfb44/revision/provisioners/var.tf
@@ -0,0 +1,16 @@
+variable "amiid" {
+    default = "ami-0faf2b6e64f313b65"
+}
+
+variable "insttype" {
+    default = "t3.micro"
+}
+
+variable "sg" {
+    default = "sg-068b3ec527f853220"
+}
+
+variable "kp" {
+    default = "key"
+}
+
