1276eaf1943c7c23fdc893d191565525996e1b23
mandade65-123 <mandade65@gmail.com>
2026-09-18 17:36:39 +0530

    s

---
+++ tfb44/revision/dependency/resource.tf
@@ -0,0 +1,64 @@
+provider "aws" {
+  region = "us-east-1"
+  profile = "configs"
+} 
+
+
+resource "aws_instance"  "webserver" {
+    ami = var.amiid
+    instance_type = var.insttype
+    vpc_security_group_ids =  [ var.sg ]
+    key_name = var.kp
+    count = 2  #identicalloops
+}
+resource "aws_security_group" "webserver_sg" {
+     depends_on = [aws_instance.webserver]  
+    name = "tf-sg-new"
+    ingress {
+      from_port = 80
+      to_port = 80
+      protocol = "TCP"
+      cidr_blocks  = ["0.0.0.0/0"]
+    }
+
+    ingress {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks  = ["0.0.0.0/0"]
+    }
+
+
+    egress {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks  = ["0.0.0.0/0"]
+
+     }
+}
+
+
+
+
+resource "aws_instance"  "webservertwo" {
+    for_each = toset(var.imageid)    #unidenticalloops
+    ami = each.value
+    instance_type = var.insttype
+    vpc_security_group_ids =  [ var.sg]
+    key_name = var.kp
+
+}
+
+
+
+
+variable "imageid" {
+   default = ["ami-0b6d9d3d33ba97d99" , "ami-02b3d83d84b07786d" , "ami-05b8af58f7410b671"]
+}
+
+output "ip" {
+    value = [
+      for amiid in var.imageid:  
+        aws_instance.webservertwo[amiid].public_ip]
+}
