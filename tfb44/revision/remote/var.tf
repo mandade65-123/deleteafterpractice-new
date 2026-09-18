7837aedd07d51fc63f3ed58291d1dcaf08f6c283
mandade65-123 <mandade65@gmail.com>
2026-09-18 10:55:55 +0530

    s

---
+++ tfb44/revision/remote/var.tf
@@ -0,0 +1,18 @@
+variable "amiid" {
+    default =  "ami-0bd3fbcdc633a1b1a"
+}
+variable "insttype" {
+    default = "t3.micro"
+}
+
+variable "sg" {
+    default = "sg-00af24a514ce5e504"
+}
+
+
+variable "kp" {
+    default = "newkey"
+}
+
+
+
