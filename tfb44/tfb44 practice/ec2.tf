26a7c182f265bd84b72b60370f5b605515e735ee
mandade65-123 <mandade65@gmail.com>
2026-09-22 12:15:49 +0530

    a

--- tfb44/tfb44 practice/ec2.tf
+++ tfb44/tfb44 practice/ec2.tf
@@ -14,6 +14,12 @@ resource "aws_instance"  "webserver" {
         purpose = "webserver"
      }
      count= 1
+      user_data = <<-EOF
+                 #!/bin/bash
+                 sudo yum install nginx -y
+                 sudo systemctl start nginx
+                EOF
+
 }
 resource "aws_security_group" "webserver_sg" {
     name = "tf-sg-new"
