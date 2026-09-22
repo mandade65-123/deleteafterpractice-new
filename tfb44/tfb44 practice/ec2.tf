a4d47169053e1fea7443cc76447fe22b0407bb95
mandade65-123 <mandade65@gmail.com>
2026-09-22 11:52:38 +0530

    s

--- tfb44/tfb44 practice/ec2.tf
+++ tfb44/tfb44 practice/ec2.tf
@@ -1,60 +1,17 @@
 provider "aws" {
   region = "ap-south-1"
-  profile = "configs"
+  access_key = "my-access-key"
+  secret_key = "my-secret-key"
 }
 
-resource "aws_instance" "webserver" {
-    ami = var.amiid
-    instance_type = var.insttype
-    vpc_security_group_ids = [var.sg,aws_security_group.webserver_sg.id, data.aws_security_group.sgdata.id]
-    key_name = var.kp
-    tags = {
+
+resource "aws_instance"  "webserver" {
+     ami = "ami-066c4849e6b3a1e3d"
+     instance_type = "t3.micro" 
+     vpc_security_group_ids = ["sg-09352767190d70e24"]
+     key_name = "key"
+     tags ={
         purpose = "webserver"
-    }
-    count = var.instno
-    disable_api_termination = var.apiterm
-      user_data = <<-EOF
-                 #!/bin/bash
-                 sudo yum install nginx -y
-                 sudo systemctl start nginx
-                EOF
-}
-
-
-resource "aws_security_group" "webserver_sg" {
-    name = "tf-sg-1"
-    ingress {
-      from_port = 80
-      to_port = 80
-      protocol = "TCP"
-      cidr_blocks  = ["0.0.0.0/0"]
-    }
-
-    ingress {
-      from_port = 0
-      to_port = 0
-      protocol = "-1"
-      cidr_blocks  = ["0.0.0.0/0"]
-    }
-
-
-    egress {
-      from_port = 0
-      to_port = 0
-      protocol = "-1"
-      cidr_blocks  = ["0.0.0.0/0"]
-
      }
-}
-
-data "aws_security_group" "sgdata" {
-    name = "default"
-}
-
-output "instaneip" {
-  value = aws_instance.webserver[0].public_ip
-}
-
-output "pubdns" {
-  value = aws_instance.webserver[0].public_dns
+     count= 1
 }
